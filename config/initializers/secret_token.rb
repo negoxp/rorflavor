fda81e8890353c252e2eba4e17369c8b4acdc52c
Jorge Flores <jorge@ievolutioned.com>
2013-08-01 20:40:39 -0500

    remote heroku first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorflavor::Application.config.secret_token = '38869d563919290e746ba35fa698ee51b6ca132455e8ab903453eb152cdb35300e87da8605263968b00ee60e7d6aec12b456f82491be425afd71a38eb3f50573'
